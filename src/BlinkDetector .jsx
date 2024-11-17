7afda0bb1025fb6d5cbab06959a8988ec726a86d
Al-Anher Provat <alanherprovat3@gmail.com>
2024-11-17 11:49:35 +0600

    .........

--- src/BlinkDetector .jsx
+++ src/BlinkDetector .jsx
@@ -220,8 +220,8 @@ const handlePlayPause = () => {
     console.log("vedio dimention",video.videoWidth,video.videoHeight)
 
     if (cameraStream){
-        targetWidth = video.videoWidth;
-        targetHeight = video.videoHeight;
+        targetWidth = 640;
+        targetHeight = 360;
 
         console.log("camera stream true!!")
     }
